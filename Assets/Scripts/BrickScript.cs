b3c5f34c214418db94cddef4154f29ef03b1abc5
netanelbenis <netnel.b.i@gmail.com>
2020-05-20 18:29:42 +0300

    add broken bricks to level 2

--- Assets/Scripts/BrickScript.cs
+++ Assets/Scripts/BrickScript.cs
@@ -6,6 +6,9 @@ public class BrickScript : MonoBehaviour
 {
     public float hitsLimit;
     private float hitsCounter=0; 
+    public Sprite BrokenGreen;
+    public Sprite BrokenBlue;
+    public Sprite BrokenRed;
 // Start is called before the first frame update
 void Start()
 {
@@ -24,6 +27,15 @@ if(other.gameObject.tag == "Ball"){
     Destroy(gameObject);
     }else
     {
+        if(this.gameObject.tag == "Green-Brick"){
+             this.gameObject.GetComponent<SpriteRenderer>().sprite=BrokenGreen;
+        }
+        if(this.gameObject.tag == "Blue-Brick"){
+             this.gameObject.GetComponent<SpriteRenderer>().sprite=BrokenBlue;
+        }
+        if(this.gameObject.tag == "Red-brick"){
+            this.gameObject.GetComponent<SpriteRenderer>().sprite=BrokenRed;
+        }
         hitsCounter++;
     }
 }   
