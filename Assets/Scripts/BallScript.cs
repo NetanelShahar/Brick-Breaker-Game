aec5ebe9fe6ee24f7b9bf365fcd77d6c5d889e59
netanelbenis <netnel.b.i@gmail.com>
2020-05-20 11:20:32 +0300

    added force to the ball

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody2D rg;
+    public float ballForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg.AddForce(new Vector2(ballForce,ballForce));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
