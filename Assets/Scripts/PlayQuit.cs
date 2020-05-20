351e02b4c277c06f6751bfed6a263a52505c8bdc
DanielAbergel <danielabergel1@gmail.com>
2020-05-20 19:43:35 +0300

    Adding Strat Scene , Quit Scene , and Return Scene

---
+++ Assets/Scripts/PlayQuit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayQuit : MonoBehaviour
+{
+    public void PlayGame () 
+    {
+        Debug.Log("Moving to Level1");
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Quit () 
+    {
+        Debug.Log("Quit the Game");
+        Application.Quit();
+    }
+}
