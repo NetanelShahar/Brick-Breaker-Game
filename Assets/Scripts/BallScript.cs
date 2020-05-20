0e80d24f0371694eb136e67eb5c943c4d39cc86c
nahamaweill <nahamaweill@gmail.com>
2020-05-20 19:06:39 +0300

    Change  between levels

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class BallScript : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float ballForce;
     bool beforeGameStarted=true;
+    public float maxBballScore;
+    public string levelName;
+    private float ballScore = 0;
     // Start is called before the first frame update
-    
+
     void Start()
     {
         
@@ -23,6 +28,20 @@ public class BallScript : MonoBehaviour
             rb.isKinematic=false;
             beforeGameStarted=false;
             rb.AddForce(new Vector2(ballForce,ballForce));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.tag == "Green-Brick" || other.gameObject.tag == "Blue-Brick" || other.gameObject.tag == "Red-brick")
+        {
+            ballScore++;
+        }
+
+        if (ballScore == maxBballScore)
+        {
+            SceneManager.LoadScene(levelName);
         }
     }
 }
